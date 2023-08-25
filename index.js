58e321784b136a0213183b4901b4189bc26da817
Erick <ericklopesalmeida8@gmail.com>
2023-08-24 21:52:24 -0300

    pra variar eu codiguei demais

---
+++ index.js
@@ -0,0 +1,36 @@
+/* EXERCÍCIO 1 DE INTERPRETAÇÃO
+function minhaFuncao(variavel) {
+	return variavel * 5
+}
+
+console.log(minhaFuncao(2))
+console.log(minhaFuncao(10))
+
+RESPOSTA = a) vai aparecer 10.
+b) se retirar os dois console.log
+não vai aparecer nada no console. */
+
+
+
+/* EXERCÍCIO 2 DE INTERPRETAÇAO
+let textoDoUsuario = prompt("Insira um texto");
+
+const outraFuncao = function(texto) {
+	return texto.toLowerCase().includes("cenoura")
+}
+
+const resposta = outraFuncao(textoDoUsuario)
+console.log(resposta)
+
+RESPOSTA = a) a função serve para colocar a frase em 
+minúsculo e também para determinar se a palavra "cenoura"
+pode ser encontrada na frase ou não.
+b) para a primeira frase vai dar true pois tem "cenoura"
+na frase, e na segunda também pois a palavra "CENOURA"
+vai ser passada para minúsculo, e a terceira vai dar
+false por estar no plural */
+
+
+
+
+
